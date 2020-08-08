2a559012f047d5998fc8305546436eb702641030
nim <nimisha.k.vijay@gmail.com>
2020-08-08 17:59:30 +0530

    made responsive, added mailchimp. todo: fix css

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,18 +3,21 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import clsx from "clsx";
 import TextField from "@material-ui/core/TextField";
+import Mailchimp from 'react-mailchimp-form';
 
 import Navbar from "../components/Navbar";
 import { landingStyles } from "../styles/Landing";
 import { Typography } from "@material-ui/core";
+import { spacing } from '@material-ui/system';
 import { ContrastTextField, PrimaryButton } from "../styles/theme";
 import earth from "../assets/gifs/earth.gif";
+import earthCropped from "../assets/gifs/earth-cropped.gif";
 
 const Landing = () => {
   const classes = landingStyles();
   return (
     <div className={classes.blackBackground}>
-      <Container maxWidth="lg" className={classes.container}>
+      <Container maxWidth="lg" className={classes.homeContainer}>
         <div>
           <Navbar />
         </div>
@@ -25,29 +28,19 @@ const Landing = () => {
             alignItems="center"
             style={{ height: "100%" }}
           >
-            <Grid container item xs={12} sm={6}>
+            <Grid container item xs={12} sm={6} style={{padding: "3rem"}}>
               <Typography
                 variant="body1"
-                className={clsx(classes.whiteText, classes.boldText)}
+                className={clsx(classes.whiteText, classes.boldText, classes.smallText)}
                 display="inline"
                 gutterBottom
-                style={{
-                  fontWeight: 200,
-                  marginBottom: 18,
-                  fontSize: "1.25rem",
-                }}
               >
                 10.10.2020 10pm IST
               </Typography>
               <Typography
                 variant="h4"
-                className={clsx(classes.whiteText, classes.boldText)}
+                className={clsx(classes.whiteText, classes.bigText)}
                 display="inline"
-                style={{
-                  fontWeight: 400,
-                  marginBottom: 36,
-                  fontSize: "2.5rem",
-                }}
               >
                 Help change climate change. <br />
                 Join us on a countdown to a zero carbon world
@@ -66,16 +59,21 @@ const Landing = () => {
               style={{ marginBottom: 18 }}
             >
               <img src={earth} alt="Earth globe gif" className={classes.gif} />
+              {/* <img src={earthCropped} alt="Earth globe gif" className={classes.gif} /> */}
             </Grid>
           </Grid>
         </div>
-        <div>
+
+
+
+         { // WE DON'T HAVE SPEAKERS YET  //
+				 /*<div>
           <Grid container justify="center" alignItems="center">
             {[
               "Bob Dylan",
               "Vinay Sateesh",
               "Not Prateek",
-              "Nim zim",
+              "Nim Zim",
               "Just Kidding",
             ].map((name) => {
               return (
@@ -93,7 +91,7 @@ const Landing = () => {
                   >
                     {name.split(" ")[0]}
                   </Typography>
-                  {/* Don't forget the space in between */}` `
+                  {/* Don't forget the space in between /}` `
                   <Typography
                     variant="body1"
                     className={clsx(classes.whiteText, classes.marginBottom18)}
@@ -105,8 +103,100 @@ const Landing = () => {
               );
             })}
           </Grid>
-        </div>
+        </div> */}
       </Container>
+
+
+
+			<div id="aboutCountdown"> 
+			<Container maxWidth="lg">
+				<Grid
+            container
+            justify="center"
+            alignItems="center"
+          >
+						<Grid
+              container
+              item
+              xs={12}
+              sm={6}
+              alignItems="flex-start"
+              justify="flex-end"
+              style={{ marginBottom: 18 }}
+            >
+							{/* this div will contain the x and the changing background image 
+									position of the x is fixed.
+									the background images will scroll?
+									overflow is hidden? no that won't work
+									add scrollhandlder? change images when window.pageyoffset > something? yes ok */}
+							<div> 
+              {/* <img src={earth} alt="Earth globe gif" className={classes.x} /> */}
+							</div>
+            </Grid>
+            <Grid container item xs={12} sm={6} style={{padding: "3rem"}}>
+              <Typography
+                variant="h4"
+                className={clsx(classes.whiteText, classes.bigText)}
+                display="inline"
+                
+              >
+                About Countdown
+              </Typography>
+							<Typography
+                variant="body1"
+                className={clsx(classes.whiteText, classes.smallText)}
+                display="inline"
+                
+              >
+                Countdown is a global initiative to champion and accelerate solutions to the climate crisis, turning 
+								ideas into action. The goal: To build a better future by cutting greenhouse gas emissions 
+								in half by 2030 in the race to a zero-carbon world – a world that is safer, cleaner and fairer for everyone.
+              </Typography>
+            </Grid>
+            
+          </Grid>
+
+				</Container>
+			</div>
+			<div id="register">
+			<Container maxWidth="lg" className={classes.registerContainer}>
+
+				{/* fix css ask vinay how grids work  */}
+				<Grid
+            container
+            justify="center"
+            alignItems="center"
+            style={{ height: "100%", display: "block" }}
+          >
+						<Typography
+                variant="h4"
+                className={clsx(classes.whiteText, classes.bigText)}
+                display="inline" 
+							>
+								#JoinTheCountdown
+						</Typography>
+						
+						{/* mailchimp material ui?? ask vinay */}
+						<Mailchimp
+							action='https://tedxpesu.us17.list-manage.com/subscribe/post?u=ba7804f5c0145f0050fc88bd4&amp;id=f7a8682e3a'
+							className={classes.register}
+        			fields={[{
+            		name: 'EMAIL',
+           			placeholder: 'Email',
+           			type: 'email',
+           			required: true
+          		}, 
+							{
+								name: 'FNAME',
+								placeholder: 'Name',
+								type: 'text',
+								required: true
+							}
+        			]}
+       			 />
+					</Grid>
+			</Container>
+			</div>
     </div>
   );
 };
--- src/styles/Landing.js
+++ src/styles/Landing.js
@@ -1,14 +1,51 @@
 import { makeStyles } from "@material-ui/core/styles";
+import PrimaryButton from "./theme"
+import theme from "./theme"
 
 //Donot remove as we need these import for some styles to take effect
 
 const landingStyles = makeStyles((theme) => ({
-  blackBackground: { backgroundColor: "black", height: "100vh" },
-  container: {
+  blackBackground: { backgroundColor: "black", overflowY: 'auto' },
+  homeContainer: {
     display: "grid",
     gridTemplateRows: "auto 1fr auto",
     height: "100vh",
   },
+	registerContainer: {
+    display: "grid",
+    gridTemplateRows: "auto 1fr auto",
+    height: "100vh",
+		[theme.breakpoints.down("md")]: {
+			height: "50vh",
+		}
+  },
+	register: {
+		// SEND HELP
+		"& input": {
+			// input form styles		
+		},
+		"& button": {
+			// how do we make this the same as the register button?!
+			// https://github.com/gndx/react-mailchimp-form/blob/master/src/index.js
+			// ^^ check 
+		}
+	},
+	smallText: {
+    fontWeight: 200,
+    marginBottom: 18,
+    fontSize: "1.25rem",
+		[theme.breakpoints.down('md')]: {
+			fontSize: "0.75rem"
+		}
+	},
+	bigText: {
+ 		fontWeight: 400,
+ 		marginBottom: 36,
+ 		fontSize: "2.5rem",
+		[theme.breakpoints.down('md')]: {
+			fontSize: "1.5rem"
+		}
+	},
   whiteText: { color: "white" },
   marginBottom1: { marginBottom: 8 },
   marginBottom18: { marginBottom: 18 },
@@ -17,7 +54,9 @@ const landingStyles = makeStyles((theme) => ({
   marginLeftRight32: { marginRight: 32, marginLeft: 32 },
   marginTop4: { marginTop: 36 },
   boldText: { fontWeight: "600" },
-  gif: { height: "100%", width: "100% " },
+  gif: { height: "100%", width: "100%", padding: "10%" },
+
+
 }));
 
 export { landingStyles };
