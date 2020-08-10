6679bf8c41e908497c0a2604bcc8eff6b9e86d6a
nim <nimisha.k.vijay@gmail.com>
2020-08-10 17:19:01 +0530

    added countdown link and changed to #join...

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -24,7 +24,7 @@ const MailchimpForm = () => {
           gutterBottom
           align="center"
         >
-          Join the countdown
+          #JoinTheCountdown
         </Typography>
         <Typography
           variant="body1"
